070ea02e73f73483c031f00ab9eb816b93cf7ca9
Gabriel <gbsm1993@gmail.com>
2023-04-16 03:21:00 -0300

    add flag for sanitizer

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 CC="clang"
-CFLAGS="-std=c99 -Werror -Wall -Wpedantic -Wconversion"
+CFLAGS="-std=c99 -Werror -Wall -Wpedantic -Wconversion -fno-omit-frame-pointer"
 
 $CC $CFLAGS ./$1/solution.c ./$1/main.c -o ./main && ./main
 
